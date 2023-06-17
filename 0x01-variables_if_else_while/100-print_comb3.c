05fc6291b4b9bd2d02260d5e3d3343ad2005979e
Jagun-molu <jonkol9jaint@gmail.com>
2023-06-17 00:28:13 -0700

    Here it goes

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+	int num;
+
+	for (number = 0; number < 9; number++)
+	{
+		for (num = 0; num <= 9; num++)
+		{
+			putchar('0' + number);
+			putchar('0' + num);
+			if (number != 8 && number != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
