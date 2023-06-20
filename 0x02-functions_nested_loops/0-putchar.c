a340603a5639d5be0b6011c795f81ef5d8c88a18
Jagun-molu <jonkol9jaint@gmail.com>
2023-06-20 11:28:22 -0700

    Welcome Back

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_putchar');
+	_putchar('\n');:
+
+	return (0);
+
+}
