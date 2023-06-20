f7499f95f1c2a56949dd12e9096dff07eec72c16
Jagun-molu <jonkol9jaint@gmail.com>
2023-06-20 12:07:59 -0700

    OK.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,19 +8,14 @@
  *
  * Return: Always 0 (Success)
  */
+
+void _puchar(char character)
+{
+	putchar(character)
+}
+
 int main(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		putchar(letter);
-	}
-	for (letter = 'A'; letter <= 'Z'; letter++)
-	{
-		putchar(letter);
-	}
-	putchar('\n');
 
 	return (0);
 
