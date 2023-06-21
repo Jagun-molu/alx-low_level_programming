b6d822df3e64122953c407fe7a141ab68c2bca4e
Jagun-molu <jonkol9jaint@gmail.com>
2023-06-21 15:19:26 -0700

    OK.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - A brief description of the function.
+ *
+ * @alphabet: the integer to be checked
+ *
+ * Return: 1 if @alphabet is lowercase else 0
+ */
+int _abs(int alphabet)
+{
+
+	if (alphabet < 0)
+	{
+		return (alphabet * -1);
+	}
+	return alphabet;
+
+}
