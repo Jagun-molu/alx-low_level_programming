9fd301a9ca56b24f7b2149e82ced42c9593aaf69
Jagun-molu <jonkol9jaint@gmail.com>
2023-06-21 20:01:39 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,17 +8,15 @@
 int main(void)
 {
 	int i;
-	unsigned long int j, k, l;
+	unsigned long int j, k, l, m;
 
 	i = 3;
 	j = 1;
 	k = 2;
 	l = 0;
-	printf("%d, %d", 1, 2);
+	m = 0;
 	while (l + k < 4000000)
 	{
-		putchar(',');
-		putchar(' ');
 		if (i == 3)
 		{
 			l = j + k;
@@ -30,7 +28,8 @@ int main(void)
 			k = l;
 			l += j;
 		}
-		printf("%lu", l);
+		if (l % 2 == 0)
+			m += l;
 	}
 	putchar('\n');
 	return (0);
