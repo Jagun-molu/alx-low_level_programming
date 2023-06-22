419e6603993072833697caff85d57c5a3166b27a
Jagun-molu <jonkol9jaint@gmail.com>
2023-06-21 17:44:49 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/testing.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * print_integer - Prints the given integer to the standard output.
+ *
+ * @a: The integer to be printed.
+ */
+void print_integer(int a)
+{
+	if (a / 10)
+	{
+		print_integer(a / 10);
+	}
+	_putchar(a % 10 + '0');
+}
+
+/**
+ * print_to_98 - Prints from @a to 98.
+ *
+ * @n: The number to begin printing from.
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			print_integer(i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			print_integer(i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
+}
