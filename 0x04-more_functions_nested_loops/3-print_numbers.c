702560edf5fe06b1d1c52b877758a905f1816565
Jagun-molu <jonkol9jaint@gmail.com>
2023-06-22 21:57:38 -0700

    print numbers function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_numbers - prints numbers from 0 to 9.
+ *
+ *
+ * Return: The product of @a and @b
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar('0' + i);
+	_putchar('\n');
+}
