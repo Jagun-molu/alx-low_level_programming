ce192fcd1426cc74da4b572e4262d6142e4347c5
Jagun-molu <jonkol9jaint@gmail.com>
2023-06-23 11:21:09 -0700

    print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,9 +15,10 @@ void print_integer(int a)
 }
 
 /**
- * print_number: prints @n to the output without using standard libraries.
+ * print_number - prints @n to the output without using standard libraries.
+ *
  * @n: the number to be printed out.
- * Description: Later
+ *
  * Return: nothing
  */
 void print_number(int n)
