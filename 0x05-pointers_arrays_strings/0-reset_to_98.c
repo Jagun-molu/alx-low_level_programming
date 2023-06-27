ba961f477b2d32d5de9b4303d01c1316a1af24c1
Jagun-molu <jonkol9jaint@gmail.com>
2023-06-26 18:00:09 -0700

    reset to 98.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * reset_to_98 - resets the value of a variable to 98
+ * @n: The pointer variable.
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
