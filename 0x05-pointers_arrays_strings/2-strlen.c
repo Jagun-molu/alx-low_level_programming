38bb17d2f34030556116e4c453d746b71211bd44
Jagun-molu <jonkol9jaint@gmail.com>
2023-06-26 18:30:09 -0700

    length of a string through pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _strlen - returns the length of string @s.
+ * @s: The string whose length is to be checked.
+ *
+ * Return: The length of the string @s
+ */
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (*s != '\0')
+	{
+		n++;
+		s++;
+	}
+	return n;
+}
