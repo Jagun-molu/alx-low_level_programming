aeb24f36fe7c0b06287f51b919bd46bef2312551
Jagun-molu <jonkol9jaint@gmail.com>
2023-06-30 16:29:35 -0700

    Task 5 - Change to UPPER.

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - Converts lowercase to UPPERCASE.
+ *
+ * @c: The string to be converted.
+ *
+ * Return: The character.
+ */
+
+char *string_toupper(char *c)
+{
+	int temp;
+
+	while (*c != '\0')
+	{
+		temp = *c;
+		if (temp >= 97 && temp <= 122)
+			*c = (char)(temp - 32);
+		c++;
+	}
+
+	return (c);
+}
