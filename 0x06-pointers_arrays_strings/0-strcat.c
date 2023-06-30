0758c8f765523a5b7295042acd036e04f3f777ab
Jagun-molu <jonkol9jaint@gmail.com>
2023-06-29 20:35:48 -0700

    String Concatenation, Task 6

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _strcat - Adds the content of @src to @dest.
+ * @src: Where to copy text from.
+ * @dest: Where to copy text to.
+ *
+ * Return: The concatenated @dest + @src.
+ */
+char *_strcat(char *dest, char *src)
+{
+
+	while (*dest)
+	{
+		dest++;
+	}
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return (dest);
+}
